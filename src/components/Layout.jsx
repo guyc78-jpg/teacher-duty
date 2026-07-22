511d13622aa745edeca946cbc1111e34fd5a8eac
Guy Cohen <guyc78@gmail.com>
2026-07-22 12:06:06 +0000

    File changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -138,11 +138,6 @@ export default function Layout() {
 
         {/* Content */}
         <main className="flex-1 min-w-0">
-          <div className="lg:hidden flex justify-end px-4 py-2 border-b border-border">
-            <button onClick={toggleDark} className="p-2">
-              {dark ? <Sun className="w-4 h-4" /> : <Moon className="w-4 h-4" />}
-            </button>
-          </div>
           <div className="p-4 lg:p-6 max-w-7xl mx-auto">
             <Outlet />
           </div>
